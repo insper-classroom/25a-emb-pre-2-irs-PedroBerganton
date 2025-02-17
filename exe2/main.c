01730f3a1b8a3423a662907dd66cbcd8d8e3e032
Pedro Berganton <pberganton@hotmail.com>
2025-02-17 19:48:54 -0300

    Ex 2

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,20 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_R = 28;
+
+volatile int inverso = 0;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, 0);
+  gpio_pull_up(BTN_PIN_R);
+
   while (true) {
+    if (gpio_get(BTN_PIN_R) == 0){
+      gpio_set_dir(BTN_PIN_R, (1-inverso));
+    }
   }
 }
