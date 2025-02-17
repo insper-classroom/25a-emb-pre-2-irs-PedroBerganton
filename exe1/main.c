6af412f11790c8873670b85c2a6aa1ae660e0665
Pedro Berganton <pberganton@hotmail.com>
2025-02-17 19:34:49 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -4,12 +4,18 @@
 
 const int BTN_PIN_R = 28;
 
+volatile int FLAG = 0;
+volatile int EVENTS1 = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    printf("fall \n");
-  } else if (events == 0x8) { // rise edge
-    printf("rise \n");
-  }
+  FLAG = 1;                                                  // mostra que evento aconteceuu
+  EVENTS1 = events;                                          // adiciona um valor a variavel compativel a fall ou rise
+
+  // if (events == 0x4) { // fall edge
+  //   printf("fall \n");
+  // } else if (events == 0x8) { // rise edge
+  //   printf("rise \n");
+  // }
 }
 
 int main() {
@@ -20,8 +26,17 @@ int main() {
   gpio_pull_up(BTN_PIN_R);
 
   gpio_set_irq_enabled_with_callback(
-      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);  //chama a funcao quando estiver em rise ou fall
 
   while (true) {
+    if (FLAG != 0){
+      FLAG = 0;                                                  // volta a flag para o padrao zero
+
+      if (EVENTS1 == 0x4) { // fall edge - isso e quando o sinal cai de 1 para 0
+        printf("fall \n");
+      } else if (EVENTS1 == 0x8) { // rise edge - aqui e quando o sinal sobe de 0 para 1
+        printf("rise \n");
+  }
+    }
   }
 }
