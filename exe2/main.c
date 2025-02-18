a6b9795b22253c7ce12be218e07f2f08c45861a0
Pedro Berganton <pberganton@hotmail.com>
2025-02-18 19:00:07 -0300

    Exs: 2 e 3

--- exe2/main.c
+++ exe2/main.c
@@ -5,34 +5,33 @@
 const int BTN_PIN_R = 28;
 const int LED = 4;
 
-volatile int FLAG_BOTAO = 0;
+volatile int FLAG_BOTAO = 1;
+volatile int sinal = 0;  
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) {         // se for um FALL, botao deve ser 1
-    FLAG_BOTAO = 1;}
-    else if (events == 0x8){  // se for um RISE, botao deve ser 0
+  if (events == 0x4) {         // se for um FALL, botao foi pressionado
     FLAG_BOTAO = 0;
-}
+  }
 }
 
 int main() {
+  // int sinal = 0;
   stdio_init_all();
 
   gpio_init(BTN_PIN_R);
   gpio_set_dir(BTN_PIN_R, GPIO_IN);
   gpio_pull_up(BTN_PIN_R);
-
-  gpio_init(LED);
-  gpio_set_dir(LED, GPIO_OUT);
-
-
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+  
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);  
 
-  int sinal = 0;
+
   while (true) {
     if (FLAG_BOTAO == 0){ //se botao estiver acionado
+      FLAG_BOTAO = 1;
       sinal = 1 - sinal;
-      gpio_set_dir(LED, (sinal));
+      gpio_put(LED, sinal);
     }
   }
 }
--- exe3/main.c
+++ exe3/main.c
@@ -5,12 +5,17 @@
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+volatile int FLAG_BOTAO_R = 1;
+volatile int FLAG_BOTAO_G = 1;
+
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
     if (gpio == BTN_PIN_R)
-      printf("fall red\n");
+      FLAG_BOTAO_R = 0;
+      // printf("fall red\n");
     else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
+      FLAG_BOTAO_G = 0;
+      // printf("fall green\n");
   }
 }
 
@@ -26,12 +31,22 @@ int main() {
   gpio_pull_up(BTN_PIN_G);
 
   // callback led r (first)
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   // callback led g (nao usar _with_callback)
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if (FLAG_BOTAO_R == 0){ //se botao estiver acionado
+        FLAG_BOTAO_R = 1;
+        sleep_ms(300);
+        printf("fall red\n");
+    }
+    if (FLAG_BOTAO_G == 0){ //se botao estiver acionado
+        FLAG_BOTAO_G = 1;
+        sleep_ms(300);
+        printf("fall green\n");
+    }
   }
+
 }
